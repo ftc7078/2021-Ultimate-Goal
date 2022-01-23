07ff415f863ece9955d77a97572c0961d8ca7268
ftc7078 <59035211+ftc7078@users.noreply.github.com>
2022-01-23 16:04:08 -0500

    2022 working

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_FreightFrenzy.java
@@ -0,0 +1,62 @@
+
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp(name="Movment_FreightFrenzy", group="Linear Opmode")
+
+public class Movment_FreightFrenzy extends LinearOpMode {
+
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private FFRobot robot = new FFRobot();
+
+    private MecanumDrive mecanumDrive = new MecanumDrive();
+
+    @Override
+    public void runOpMode() {
+        mecanumDrive.init(hardwareMap, telemetry, this);
+        robot.init(hardwareMap,telemetry,this);
+
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            double speed = 1;
+
+             speed = (gamepad1.right_trigger * 0.3) + 0.7;
+            double fwd = gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double rot= gamepad1.right_stick_x;
+
+            fwd = fwd * speed;
+            strafe =strafe * speed * 1.6;
+            if (strafe > 1) {
+                strafe = 1;
+            } else if (strafe < -1) {
+                strafe = -1;
+            }
+            rot = rot * speed;
+            mecanumDrive.setMotors(strafe,fwd,rot, 1);
+
+            mecanumDrive.tickSleep();
+            //telemetry.addData("Left/Right Stick", "LX (%.2f), LY (%.2f), RX (%.2f), RY (%.2f)", gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.right_stick_y);
+
+            telemetry.update();
+        }
+
+    }
+
+}
+r
