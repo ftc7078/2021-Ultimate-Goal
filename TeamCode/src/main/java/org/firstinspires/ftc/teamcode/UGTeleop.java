a8e40de8b20aa50f553fd813ea25aa57c1f50d06
ftc7078 <59035211+ftc7078@users.noreply.github.com>
2022-01-24 18:27:45 -0500

    Fixes for deadzone again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UGTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UGTeleop.java
@@ -42,7 +42,6 @@ public class UGTeleop extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            gamepad1.setJoystickDeadzone((float)0.2);
             double speed = 1;
 
              speed = (gamepad1.right_trigger * 0.6) + 0.4;
